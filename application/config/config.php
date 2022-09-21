8b4ab97f679fd2d86d2e26a032ddfd0c334e0902
Mr.Nuk <supaporn.dev@gmail.com>
2022-09-21 11:12:09 +0700

    changed base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/biosafety-medtu-ci';
+$config['base_url'] = 'http://localhost/login-and-adminltetemplate';
 
 /*
 |--------------------------------------------------------------------------
